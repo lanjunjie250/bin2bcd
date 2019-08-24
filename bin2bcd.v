3e8b219b7ab0e769d440e5e0016e736ecd23fb12
lanjunjie <lanjunjie250@outlook.com>
2019-08-24 14:32:53 +0800

    wrote a readme file

---
+++ bin2bcd.v
@@ -0,0 +1,14 @@
+/******************************
+* file   : bin2bcd.v
+* date   : 2019-08-24
+* author : lanjunjie250@outlook.com 
+******************************/
+
+module bin2bcd(
+
+
+);
+
+
+
+endmodule //bin2bcd
