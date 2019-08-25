77bea3bae874ac62f9faa06c81b92ba1e45fa181
lanjunjie <lanjunjie250@outlook.com>
2019-08-25 11:50:55 +0800

    更改目录结构

---
+++ verilog/top.v
@@ -0,0 +1,14 @@
+/*********************************
+* file 	 : top.v
+* date   : 2019-08-24
+* auther : lanjunjie250@outlook.com
+*********************************/
+
+module top();
+
+initial begin
+    $display("this is top level");
+    $finish;
+end
+
+endmodule //top
